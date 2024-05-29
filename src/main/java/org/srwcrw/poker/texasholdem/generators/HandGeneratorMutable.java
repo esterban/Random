e3ed8f6b2a04034574e01c62f34bc1168b00a5db
stepwrig <stephen.wright79@gmail.com>
2024-05-29 13:35:37 +0100

    Updating .gitignore files

---
+++ src/main/java/org/srwcrw/poker/texasholdem/generators/HandGeneratorMutable.java
@@ -0,0 +1,12 @@
+//package org.srwcrw.poker.texasholdem.generators;
+//
+//import org.srwcrw.poker.texasholdem.collections.IPack;
+//
+//import java.util.AbstractMap;
+//
+//public class HandGeneratorMutable implements IHandGenerator {
+//    @Override
+//    public AbstractMap.SimpleEntry<IPack, IPack> generateHandAndRemoveImmutable(IPack pack, int numberOfCards) {
+//        return null;
+//    }
+//}
